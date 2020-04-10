bc7774fcba2f0ec273d5aef3e2954c960e5e3c35
ann-eng <62693920+ann-eng@users.noreply.github.com>
2020-04-10 09:47:34 +0300

    Update Command.java

--- Command.java
+++ Command.java
@@ -10,38 +10,72 @@ import java.io.*;
 public class Command implements CommandsList{
     /**Сохраняет историю команд.*/
     private History historyList;
+    /**Поток информации с консоли.*/
+    private BufferedReader reader;
+    /**Поток информации из файла.*/
+    private BufferedInputStream fileReader;
+    /**Флаг для определения обрабатываемого потока.*/
+    private int streamID;
     /**
      * Сохраняет количество вызовов команды execute_script.
      * */
     private int countOfScripts;
+    /**
+     * Коллекция {@link AllProducts}, которую необходимо обработать
+     * */
+    private AllProducts all;
+
     /**Стандартный конструктор.*/
     public Command(){
         historyList = new History();
         countOfScripts = 0;
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        all = new AllProducts();
+        streamID = 0;
     }
+
+    /**Конструктор с сохранением коллекции.*/
+    public Command(AllProducts a){
+        historyList = new History();
+        countOfScripts = 0;
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        all = a;
+        streamID = 0;
+    }
+
     /**
      * Выводит историю команд.
      * */
-    public void history(){ historyList.getHistory(); }
+    public void history(){
+        historyList.getHistory();
+    }
     /**
      * Выполняет команду {@code execute_script}.
-     * @param all коллекция {@link AllProducts}, которую необходимо обработать
      * @param s имя файла, в котором хранятся команды
      * */
-    public void executeScript(AllProducts all, String s) {
+    public void executeScript( String s) {
         countOfScripts++;
         if (countOfScripts <= 20) {
             File file = new File(s.trim());
-            try (BufferedInputStream reader = new BufferedInputStream(new FileInputStream(file))) {
+            try  {
+                fileReader = new BufferedInputStream(new FileInputStream(file));
                 StringBuilder command = new StringBuilder();
                 int i;
+                int lineNumber = 0;
                 do {
-                    i = reader.read();
+                    i = fileReader.read();
                     if (i != -1) {
                         if (i == 10){
-                            execution(all, command.toString());
+                            lineNumber++;
+                            try {
+                                streamID = 1;
+                                execution(command.toString());
+                            }catch (UknownCommandException | UncorrectArgumentException e){
+                                System.out.println("Ошибка в строке " + lineNumber+ ".");
+                                System.out.println( e.getMessage());
+                            }
                             command.delete(0, command.length());
-                        }else command.append((char) i);
+                        }else if (i != 13) command.append((char) i);
                     }
                 } while(i != -1);
             } catch (FileNotFoundException e) {
@@ -49,21 +83,20 @@ public class Command implements CommandsList{
             } catch (SecurityException e) {
                 System.out.println("Нет доступа к файлу.");
             } catch (IOException e) {
-                System.out.println("Ввод-вывод не удался.");
+                System.out.println("Чтение не удалось.");
             }
         }
     }
     /**
      * Вызывает заданные команды.
      *<p>Проверяет на корректность вводимые команды, выполняет команды с помощью {@link CommandsList}.</p>
-     * @param all коллекция {@link AllProducts}, которую необходимо обработать
      * @param s имя файла, в котором хранятся команды
      * */
-    public void execution(AllProducts all, String s) throws UknownCommandException{
-        historyList.add(s);
-        String[] message = s.trim().split(" ");
-        if (message.length == 1){
-            switch (message[0]){
+    public void execution(String s) throws UknownCommandException, UncorrectArgumentException{
+        String[] task = s.split(" ");
+        Product p = new Product();
+        if (!task[0].equals("")) historyList.add(task[0]);
+        switch (task[0]){
                 case "help":
                     help();
                     break;
@@ -86,43 +119,175 @@ public class Command implements CommandsList{
                     all.findMinName();
                     break;
                 case "":
-                    System.out.println("Введите команду:");
                     break;
-                default:
-                    throw new UknownCommandException(s);
-            }
-        }else if (message.length == 2){
-            switch (message[0]){
                 case "remove_lower":
-                    removeLower(all);
+                    p.create(reader);
+                    removeLower(p, all);
                     break;
                 case "remove_key":
-                    removeKey(all, message[1]);
+                    removeKey(all, s.replaceFirst("remove_key", "").trim());
                     break;
                 case "execute_script":
-                    executeScript(all, message[1]);
+                    executeScript(s.replaceFirst("execute_script", "").trim());
                     break;
                 case  "count_less_than_manufacturer":
-                    countLessThanManufacturer(all);
+                    Organization o = new Organization();
+                    if (streamID == 0) o.create(reader);
+                    if (streamID == 1) o.create(readFromFileOrganization());
+                    countLessThanManufacturer(o, all);
                     break;
                 case "print_field_descending_part_number":
                     printFieldDescendingPartNumber(all, "PartNumber");
                     break;
                 case "insert":
-                    insert(all, message[1]);
-                    break;
+                    s = s.replaceFirst("insert", "").trim();
+                    if (s.matches("\\d+")){
+                        if (Long.decode(s) > 0){
+                            if (streamID == 0) p.create(reader);
+                            if (streamID == 1) p.create(readFromFileProduct());
+                            all.setProduct(Long.decode(s), p);
+                            System.out.println("Продукт добавлен в коллекцию.");
+                            break;
+                        }else throw new UncorrectArgumentException("Индекса " + s + " не существует.");
+                    }else  throw  new UncorrectArgumentException("Аргумент команды введен неверно.");
                 case "update":
-                    update(all, message[1]);
-                    break;
+                    s = s.replaceFirst("update", "").trim();
+                    if (s.matches("\\d+")){
+                        if (all.getProdcts().containsKey(Long.decode(s))){
+                            if (streamID == 0) p.create(reader);
+                            if (streamID == 1) p.create(readFromFileProduct());
+                            all.setProduct(Long.decode(s), p);
+                            System.out.println("Продукт " + s + " обновлен.");
+                            break;
+                        }else throw new UncorrectArgumentException("В коллекции нет элемента с индексом " + s + ".");
+                    }else throw new UncorrectArgumentException("Аргумент команды введен неверно.");
                 case "replace_if_lowe":
-                    replaceIfLowe(all, message[1]);
-                    break;
-
+                    s = s.replaceFirst("replace_if_lowe", "").trim();
+                    if (s.matches("\\d+")){
+                        if (all.getProdcts().containsKey(Long.decode(s))){
+                            if (streamID == 0) p.create(reader);
+                            if (streamID == 1) p.create(readFromFileProduct());
+                            if (all.getProdcts().get(Long.decode(s)).compareTo(p) < 0) {
+                                all.setProduct(Long.decode(s), p);
+                                System.out.println("Элемент " + s + "заменен.");
+                                break;
+                            }
+                        }else throw  new UncorrectArgumentException("В коллекции нет элемента с индексом " + s + ".");
+                    }else throw  new UncorrectArgumentException("Аргумент команды введен неверно.");
                 default:
                     throw new UknownCommandException(s);
             }
-        } else throw new UknownCommandException(s);
     }
 
-
+    /**
+     * Читает команды пользователя с консоли.
+     * */
+    public void work(){
+        try {
+            String message;
+            while (!(message = reader.readLine()).matches("[ ]*exit[ ]*")){
+                if (message == null) message = "";
+                else message = message.trim();
+                try {
+                    streamID = 0;
+                    execution(message);
+                }catch (UknownCommandException | UncorrectArgumentException e) {
+                    System.out.println(e.getMessage());
+                }
+                System.out.print(": ");
+            }
+            System.out.println("Вход из программы. Завершение работы.");
+            reader.close();
+        }catch (java.lang.NullPointerException e){
+            System.out.println("Завершение работы программы.");
+        }catch (IOException e) {
+            System.out.println("Завершение работы программы...");
+        }
+    }
+    /**
+     * Находит информацию о продукте в файле.
+     * @return список полей
+     * */
+    public String[] readFromFileProduct(){
+        String[] options = new String[10];
+        try  {
+            StringBuilder item = new StringBuilder();
+            int i;
+            String s;
+            do {
+                i = fileReader.read();
+                if (i != -1) {
+                    if (i == 10){
+                        if (item.toString().matches("[ ]*[Ии]мя продукта[ ]*:[ ]*\\w+[ ]*")){
+                            options[0] = item.toString().replaceFirst("[Ии]мя продукта[ ]*:", "").trim();
+                        }else if (item.toString().matches("[ ]*[Кк]оордината[ ]+[Xx][ ]*:\\d+[ ]*")){
+                            options[1] = item.toString().replaceAll("[Ккординат :]+", "");
+                        }else if (item.toString().matches("[ ]*[Кк]оордината[ ]+[Yy][ ]*:\\d+[ ]*")){
+                            options[2] = item.toString().replaceAll("[ :Ккординат]+", "");
+                        }else if (item.toString().matches("[ ]*[цЦ]ена[ ]*продукта[ ]*:[ ]*\\d*[ ]*")){
+                            options[3] = item.toString().replaceAll("[Цценапродукт: ]", "");
+                        }else if (item.toString().matches("[ ]*[Ии]нвентарный[ ]+номер[ ]*:[ ]*\\w*[ ]*")){
+                            s = item.toString().replaceFirst("[Ии]нвентарный[ ]+номер[ ]*:", "").trim();
+                            if (s.equals("") || s.length() > 15) options[4] = s;
+                        }else if (item.toString().matches("[ ]*[Сс]тоимость[ ]+изготовления[ ]*:[ ]*\\d+[ ]*")){
+                            options[5] = item.toString().replaceAll("[ :Сстоимьзгвленя]", "");
+                        }else if (item.toString().matches("[ ]*[Ее]диницы[ ]+измерения[ ]*:[ ]*\\w+[ ]*")){
+                           s = item.toString().replaceAll("[ :Еединцызмря]", "");
+                            if (UnitOfMeasure.isCorrect(s)) options[6] = s;
+                        }else if (item.toString().matches("[ ]*[Нн]азвание[ ]+производителя[ ]*:[ ]*\\w+[ ]*")){
+                            options[7] = item.toString().replaceFirst("[ ]*[Нн]азвание[ ]+производителя[ ]*:[ ]*", "").trim();
+                        }else if (item.toString().matches("[ ]*[Гг]одовой[ ]+оборот[ ]+производителя[ ]*:[ ]*\\d+[ ]*")){
+                            options[8] = item.toString().replaceAll("[: Ггодвйбртпизеля]+", "");
+                        }else if (item.toString().matches("[ ]*[Тт]ип[ ]+организации[ ]*:[ ]*\\w+[ ]*")){
+                            s = item.toString().replaceAll("[ :Ттипорганзц]+", "");
+                            if (OrganizationType.isCorrect(s)) options[9] = s;
+                        } else return options;
+                        item.delete(0, item.length());
+                    }else if (i != 13) item.append((char) i);
+                }
+            } while(i != -1);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден.");
+        } catch (SecurityException e) {
+            System.out.println("Нет доступа к файлу.");
+        } catch (IOException e) {
+            System.out.println("Чтение не удалось.");
+        }
+        return options;
+    }
+    /**
+     * Находит информацию об организации в файле.
+     * @return список полей
+     * */
+    public String[] readFromFileOrganization(){
+        String[] options = new String[3];
+        try  {
+            StringBuilder item = new StringBuilder();
+            int i;
+            String s;
+            do {
+                i = fileReader.read();
+                if (i != -1) {
+                    if (i == 10){
+                        if (item.toString().matches("[ ]*[Нн]азвание[ ]+производителя[ ]*:[ ]*\\w+[ ]*")){
+                            options[0] = item.toString().replaceFirst("[ ]*[Нн]азвание[ ]+производителя[ ]*:[ ]*", "").trim();
+                        }else if (item.toString().matches("[ ]*[Гг]одовой[ ]+оборот[ ]+производителя[ ]*:[ ]*\\d+[ ]*")){
+                            options[1] = item.toString().replaceAll("[: Ггодвйбртпизеля]+", "");
+                        }else if (item.toString().matches("[ ]*[Тт]ип[ ]+организации[ ]*:[ ]*\\w+[ ]*")){
+                            s = item.toString().replaceAll("[ :Ттипорганзц]+", "");
+                            if (OrganizationType.isCorrect(s)) options[2] = s;
+                        } else return options;
+                        item.delete(0, item.length());
+                    }else if (i != 13) item.append((char) i);
+                }
+            } while(i != -1);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден.");
+        } catch (SecurityException e) {
+            System.out.println("Нет доступа к файлу.");
+        } catch (IOException e) {
+            System.out.println("Чтение не удалось.");
+        }
+        return options;
+    }
 }
