3537a045c56216552ace773b9c19e26e3b3c095c
ann-eng <62693920+ann-eng@users.noreply.github.com>
2020-04-10 09:47:03 +0300

    Update CommandsList.java

--- CommandsList.java
+++ CommandsList.java
@@ -20,8 +20,7 @@ public interface CommandsList {
      * */
     default void info(AllProducts all){
         System.out.println("Тип данной коллекции: TreeMap");
-        Product p = all.getProdcts().get(1L);
-        if (p != null) System.out.println("Дата инициализации: " + p.getCreationDate().toString());
+        System.out.println("Дата инициализации: " + all.getCreationDate());
         System.out.println("Количество элементов: " + all.getProdcts().size());
     }
     /**
@@ -29,48 +28,16 @@ public interface CommandsList {
      * @param all класс {@link AllProducts}, хранящий коллекцию
      * */
     default void show(AllProducts all) {
-        try {
+        if (all.getCollection().size() == 0) System.out.println("Коллекция пустая.");
+        else {
             for(Product p : all.getCollection()){
                 p.getInfo();
                 System.out.println("-----------------");
             }
-        }catch (NullPointerException e){
-            System.out.println("Коллекция пустая.");
         }
+    }
 
-    }
-    /**
-     *  Команда {@code insert}. Добавляет элемент в рабочую коллекцию.
-     * @param all класс {@link AllProducts}, хранящий коллекцию
-     * @param s строка, содержащая Id вводимого объекта
-     * @throws UncorrectArgumentException , если введенные аргументы неверны
-     * */
-    default void insert(AllProducts all, String s) throws UncorrectArgumentException{
-        if (s.matches("\\d+")){
-            if (Long.decode(s) > 0){
-                Product p = new Product();
-                p.create();
-                all.setProduct(Long.decode(s), p);
-                System.out.println("Продукт добавлен в коллекцию.");
-            }else throw new UncorrectArgumentException("Индекса " + s + " не существует.");
-        }else  throw  new UncorrectArgumentException("Аргумент команды введен неверно.");
-    }
-    /**
-     *  Команда {@code update}. Обновляет элемент в рабочей коллекции.
-     * @param all класс {@link AllProducts}, хранящий коллекцию
-     * @param s строка, содержащая Id обновляемого объекта
-     * @throws UncorrectArgumentException , если введенные аргументы неверны
-     * */
-    default void update(AllProducts all, String s) throws UncorrectArgumentException{
-        if (s.matches("\\d+")){
-            if (all.getProdcts().containsKey(Long.decode(s))){
-                Product p = new Product();
-                p.create();
-                all.setProduct(Long.decode(s), p);
-                System.out.println("Продукт " + s + " обновлен.");
-            }else throw new UncorrectArgumentException("В коллекции нет элемента с индексом " + s + ".");
-        }else throw new UncorrectArgumentException("Аргумент команды введен неверно.");
-    }
+
     /**
      * Команда {@code remowe_key}. Удаляет элемент в рабочей коллекции.
      * @param all класс {@link AllProducts}, хранящий коллекцию
@@ -126,56 +93,29 @@ public interface CommandsList {
     }
 
     /**
-     *  Команда {@code remowe_lower}. Удаляет все элементы коллекции, меньшие, чем заданный
-     * @param all обрабатываемая коллекция
-     * */
-    default void removeLower(AllProducts all) {
-       Product p = new Product();
-       p.create();
+     * Команда {@code remowe_lower}. Удаляет все элементы коллекции, меньшие, чем заданный
+     * @param prod сравниваемый обьект
+     * @param all обрабатываемая коллекция */
+    default void removeLower(Product prod, AllProducts all) {
        Collection<Product> collection = all.getProdcts().values();
        if (collection.size() > 0){
            for (Product o : collection){
-               if (o.compareTo(p) < 0){
+               if (o.compareTo(prod) < 0){
                    all.remove(o.getId());
                }
            }
        }
-        System.out.println("Все элементы, ниже заданного, удалены.");
+       System.out.println("Все элементы, ниже заданного, удалены.");
     }
 
     /**
-     *  Команда {@code replace_if_lowe}. Заменяет элемент коллекции, на заданный, если он меньше
-     * @param all обрабатываемая коллекция
-     * @param s Id заменяемого элемента
-     * @throws UncorrectArgumentException , если введенные аргументы неверны
-     * */
-    default void replaceIfLowe(AllProducts all, String s) throws UncorrectArgumentException{
-        if (s.matches("\\d+")){
-            if (all.getProdcts().containsKey(Long.decode(s))){
-                Product p = new Product();
-                p.create();
-                if (all.getProdcts().get(Long.decode(s)).compareTo(p) < 0) {
-                    all.setProduct(Long.decode(s), p);
-                    System.out.println("Элемент заменен.");
-                }
-            }else throw  new UncorrectArgumentException("В коллекции нет элемента с индексом " + s + ".");
-        }else throw  new UncorrectArgumentException("Аргумент команды введен неверно.");
-
-    }
-
-    /**
-     *  Команда {@code count_less_than_manufacturer}. Выводит количество элементов, значение организации которых меньше заданного
-     * @param all обрабатываемая коллекция
-     * */
-    default void countLessThanManufacturer(AllProducts all) {
-        Organization o =new Organization();
-        o.create();
-        if (all.getCollection().size() == 0) System.out.println("Количество элементов, меньше заданного равно 0.");
-        else {
-            int i = 0;
-            for (Product p : all.getCollection())  if (p.getManufacturer().compareTo(o) < 0) i++;
-            System.out.println("Количество элементов, меньше заданного равно " + i + ".");
-        }
+     * Команда {@code count_less_than_manufacturer}. Выводит количество элементов, значение организации которых меньше заданного
+     * @param o сравниваемый объект
+     * @param all обрабатываемая коллекция */
+    default void countLessThanManufacturer(Organization o, AllProducts all) {
+        int i = 0;
+        if (all.getCollection().size() > 0) for (Product p : all.getCollection())  if (p.getManufacturer().compareTo(o) < 0) i++;
+        System.out.println("Количество элементов, меньше заданного равно " + i + ".");
     }
 
     /**
